5cdb77666e017f6cf884b2baf615835aa186762d
hoyss <hoooys112@gmail.com>
2021-08-15 00:21:06 +0900

    swccode

--- app/src/main/java/com/example/swc_fin/Frag1.java
+++ app/src/main/java/com/example/swc_fin/Frag1.java
@@ -1,7 +1,5 @@
 package com.example.swc_fin;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +7,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.fragment.app.Fragment;
 
 public class Frag1 extends Fragment {
@@ -25,15 +21,12 @@ public class Frag1 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag1, container, false);
 
-        Button btn_upload = (Button) view.findViewById(R.id.btn_upload);
-        btn_upload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText et_today = (EditText) view.findViewById(R.id.et_today);
-                str += " \n " + et_today.getText().toString();
-                TextView tV_today = (TextView) view.findViewById(R.id.tV_today);
-                tV_today.setText(str);
-            }
+        Button btn_upload = view.findViewById(R.id.btn_upload);
+        btn_upload.setOnClickListener(v -> {
+            EditText et_today = view.findViewById(R.id.et_today);
+            str += " \n " + et_today.getText().toString();
+            TextView tV_today = view.findViewById(R.id.tV_today);
+            tV_today.setText(str);
         });
         return view;
     }
--- app/src/main/java/com/example/swc_fin/Frag3.java
+++ app/src/main/java/com/example/swc_fin/Frag3.java
@@ -1,6 +1,5 @@
 package com.example.swc_fin;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -22,21 +20,17 @@ public class Frag3 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag3, container, false);
 
-        EditText name = (EditText) view.findViewById(R.id.name);
-        EditText content = (EditText) view.findViewById(R.id.content);
-        TextView name_textview = (TextView) view.findViewById(R.id.name_textview);
-        TextView content_textview = (TextView) view.findViewById(R.id.content_textview);
-        Button btn_edit = (Button) view.findViewById(R.id.btn_edit);
-        btn_edit.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                String str_name = name.getText().toString();
-                name_textview.setText("작성자:" +str_name);
-                String str_content = content.getText().toString();
-                content_textview.setText(str_content);
+        EditText name = view.findViewById(R.id.name);
+        EditText content = view.findViewById(R.id.content);
+        TextView name_textview = view.findViewById(R.id.name_textview);
+        TextView content_textview = view.findViewById(R.id.content_textview);
+        Button btn_edit = view.findViewById(R.id.btn_edit);
+        btn_edit.setOnClickListener(v -> {
+            String str_name = name.getText().toString();
+            name_textview.setText("작성자:" +str_name);
+            String str_content = content.getText().toString();
+            content_textview.setText(str_content);
 
-            }
         });
         return view;
     }
